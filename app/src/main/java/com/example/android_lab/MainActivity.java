bb0b3233f8491817e74f6f7815fc575058ce2bdf
Rafal Pytel <rafalp16a@gmail.com>
2019-05-08 12:12:25 +0200

    add listView

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.android_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] values = new String[] { "Pies", "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp", "Osioł", "Chomik", "Mysz", "Jeż", "Karaluch" };
+        this.target = new ArrayList<>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+        ListView listview = findViewById( R.id.listView );
+        listview.setAdapter(this.adapter);
     }
 
-    //TODO
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
 }
